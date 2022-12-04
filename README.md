c29ea304e67aac5539ab67d740f1fed052a2a688
rattleSSnake <rattleSSnake@protonmail.com>
2022-12-04 11:10:56 +0100

    update

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # DM-casse-briques
+
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](LICENSE)
 
-*To test the game online click [here](https://kitao.github.io/pyxel/wasm/launcher/?run=rattlessnake.DM-casse-briques.game).*
+_To test the game online click [here](https://kitao.github.io/pyxel/wasm/launcher/?run=rattlessnake.DM-casse-briques.game)._
+
+## Les types de rebonds
+
+Comme demandé, le plateau dispose de 3 types de rebonds differents. Lorsque la balle atterit sur la surface horizontale du plateau, elle repart avec un angle opposé par rapport à la normale du plateau. Lorsque elle atterit sur les coins, elle repart dans la direction opposée. Enfin si elle atterit sur la partie verticale du plateau elle repart avec un angle opposé par rapport à la droite parallèle au plateau.
--- game.py
+++ game.py
@@ -1,104 +1,176 @@
+from random import choice
 import pyxel
+
+
+class Player:
+    def __init__(self):
+        self.x = pyxel.width / 2 - 40
+        self.y = pyxel.height - 20
+        self.width = 80
+        self.height = 10
+        self.color = pyxel.COLOR_GRAY
+
+    def update(self):
+        self.control()
+        self.bound()
+
+    def control(self):
+        if pyxel.btn(pyxel.KEY_RIGHT):
+            self.x += 10
+        if pyxel.btn(pyxel.KEY_LEFT):
+            self.x -= 10
+
+    def bound(self):
+        if self.x < 0:
+            self.x = 0
+        if self.x >= pyxel.width - self.width:
+            self.x = pyxel.width - self.width
+
+    def draw(self):
+        pyxel.rect(self.x, self.y, self.width, self.height, self.color)
+
+
+class Bricks:
+    def __init__(self):
+        self.width = 30
+        self.height = 14
+        self.layout = self.generateBricks()
+
+    def generateBricks(self) -> list:
+        bricks = []
+        for y in range(60, 380, self.height + 1):
+            for x in range(60, 550, self.width + 1):
+                bricks.append(
+                    {
+                        "x": x if y % 2 == 0 else x + self.width / 2,
+                        "y": y,
+                        "color": self.colorBrick(x),
+                    }
+                )
+        return bricks
+
+    def colorBrick(self, n) -> int:
+        if n % 2 == 0:
+            return pyxel.COLOR_CYAN
+        elif n % 3 == 0:
+            return pyxel.COLOR_RED
+        else:
+            return pyxel.COLOR_LIME
+
+    def draw(self):
+        for dic in self.layout:
+            pyxel.rect(dic["x"], dic["y"], self.width, self.height, dic["color"])
+
+
+class Ball:
+    def __init__(self):
+        self.x = pyxel.width / 2
+        self.y = pyxel.height - 30
+        self.radius = 8
+        self.trajectory = {"x": choice([-1, 1]), "y": -1}
+        self.speed = 2
+        self.color = pyxel.COLOR_WHITE
+
+    def update(self):
+        self.wallCollision()
+        self.x += self.trajectory["x"] * self.speed
+        self.y += self.trajectory["y"] * self.speed
+
+    def wallCollision(self):
+        if self.x < self.radius or self.x >= pyxel.width - self.radius:
+            self.trajectory["x"] = -self.trajectory["x"]
+        if self.y < self.radius:
+            self.trajectory["y"] = -self.trajectory["y"]
+
+    def collision(self, x, y, width, height):
+        """detects collision between a circle and a rectangle"""
+        distance = {
+            "x": abs(self.x - x - width / 2),
+            "y": abs(self.y - y - height / 2),
+        }
+
+        distance["corner"] = (distance["x"] - width / 2) ** 2 + (
+            distance["y"] - height / 2
+        ) ** 2
+
+        if not (
+            distance["x"] > width / 2 + self.radius
+            or distance["y"] > height / 2 + self.radius
+        ):
+            if distance["x"] <= width / 2:
+                self.trajectory["y"] = -self.trajectory["y"]
+                return True
+            if distance["y"] <= height / 2:
+                self.trajectory["x"] = -self.trajectory["x"]
+                return True
+
+        if distance["corner"] <= self.radius**2:
+            self.trajectory["x"] = -self.trajectory["x"]
+            self.trajectory["y"] = -self.trajectory["y"]
+            return True
+
+    def draw(self):
+        pyxel.circ(self.x, self.y, self.radius, self.color)
 
 
 class Game:
     def __init__(self):
         pyxel.init(640, 512, title="Brick Breaker", fps=60)
-        self.bricks = generateBricks(pyxel.width, pyxel.height)
-        self.ball = {"x": pyxel.width / 2, "y": pyxel.height - 30, "radius": 8}
-        self.player = {"x": pyxel.width / 2 - 25, "y": pyxel.height - 20}
+        self.start = False
+        self.player = Player()
+        self.bricks = Bricks()
+        self.ball = Ball()
+        pyxel.sound(0).set("a1", "t", "7742", "v", 8)
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        self.handleBoard()
-        self.handleBall()
+        if pyxel.btnp(pyxel.KEY_RETURN):
+            self.start = True
+        if self.start == True:
+            self.player.update()
+            self.ball.update()
+            self.ball.speed = round(2 + (352 - len(self.bricks.layout)) / 352 * 3, 1)
+            self.handleCollisions()
+            self.detectWin()
+            self.detectGameOver()
 
-    def handleBoard(self):
-        if pyxel.btn(pyxel.KEY_RIGHT):
-            self.player.update({"x": self.player["x"] + 10})
-        if pyxel.btn(pyxel.KEY_LEFT):
-            self.player.update({"x": self.player["x"] - 10})
-
-        if self.player["x"] < 0:
-            self.player.update({"x": 0})
-        if self.player["x"] >= pyxel.width:
-            self.player.update({"x": pyxel.width - 10})
-
-    def handleBall(self):
-        movement = {"y": self.ball["y"] - 10}
-        if self.ball["x"] < self.ball["radius"]:
-            self.ball.update({"x": self.ball["radius"]})
-        if self.ball["y"] < self.ball["radius"]:
-            movement = {"y": self.ball["y"] + 10}
-        if self.ball["x"] >= pyxel.width - self.ball["radius"]:
-            self.ball.update({"x": pyxel.width - self.ball["radius"]})
-        if self.ball["y"] >= pyxel.height - self.ball["radius"]:
-            self.ball.update({"x": pyxel.width / 2, "y": pyxel.height - 30})
-
-        self.ball.update(movement)
-
-        for dic in self.bricks:
-            if collision(
-                self.ball,
-                {
-                    "x": dic["x"],
-                    "y": dic["y"],
-                    "width": dic["width"],
-                    "height": dic["height"],
-                },
+    def handleCollisions(self):
+        self.ball.collision(
+            self.player.x, self.player.y, self.player.width, self.player.height
+        )
+        for dic in self.bricks.layout:
+            if self.ball.collision(
+                dic["x"], dic["y"], self.bricks.width, self.bricks.height
             ):
-                if dic["color"] == 11:
-                    self.bricks.remove(dic)
-                if dic["color"] == 12:
-                    dic["color"] = 11
-                if dic["color"] == 8:
-                    dic["color"] = 12
+                pyxel.play(0, 0)
+                if dic["color"] == pyxel.COLOR_LIME:
+                    self.bricks.layout.remove(dic)
+                if dic["color"] == pyxel.COLOR_CYAN:
+                    dic["color"] = pyxel.COLOR_LIME
+                if dic["color"] == pyxel.COLOR_RED:
+                    dic["color"] = pyxel.COLOR_CYAN
+
+    def detectWin(self):
+        if len(self.bricks.layout) == 0:
+            pyxel.text(10, 10, "You Won!", 7)
+            pyxel.quit()
+
+    def detectGameOver(self):
+        if self.ball.y >= pyxel.height - self.ball.radius:
+            # self.ball.trajectory["y"] = -self.ball.trajectory["y"]
+            self.start = False
+            self.bricks.layout = self.bricks.generateBricks()
+            self.ball.x = pyxel.width / 2
+            self.ball.y = pyxel.height - 30
+            self.ball.trajectory = {"x": choice([-1, 1]), "y": -1}
+            self.player.x = pyxel.width / 2 - 40
+            self.player.y = pyxel.height - 20
 
     def draw(self):
         pyxel.cls(0)
-        for dic in self.bricks:
-            pyxel.rect(dic["x"], dic["y"], dic["width"], dic["height"], dic["color"])
-        pyxel.circ(self.ball["x"], self.ball["y"], self.ball["radius"], 7)
-        pyxel.rect(self.player["x"], self.player["y"], 52, 12, 4)
-
-
-def generateBricks(width, height):
-    brick_width = 30
-    brick_height = 14
-    positionBricks = [
-        {
-            "x": x if y % 2 == 0 else x + brick_width / 2,
-            "y": y,
-            "width": brick_width,
-            "height": brick_height,
-        }
-        for y in range(int(height / 8), int(height - 2 * height / 8), brick_height + 1)
-        for x in range(int(width / 8), int(width - width / 8), brick_width + 1)
-    ]
-    bricks = []
-    for index, dic in enumerate(positionBricks):
-        bricks.append(
-            {**dic, "color": 12 if index % 2 == 0 else (8 if index % 3 == 0 else 11)}
-        )
-    return bricks
-
-
-def collision(circle: dict, rectangle: dict):
-    xdistance = abs(circle["x"] - rectangle["x"] - rectangle["width"] / 2)
-    ydistance = abs(circle["y"] - rectangle["y"] - rectangle["height"] / 2)
-
-    if (
-        xdistance > (rectangle["width"] / 2 + circle["radius"])
-        or ydistance > rectangle["height"] / 2 + circle["radius"]
-    ):
-        return False
-
-    if xdistance <= (rectangle["width"] / 2) or ydistance <= rectangle["height"] / 2:
-        return True
-
-    return (xdistance - rectangle["x"] / 2) ** 2 + (
-        ydistance - rectangle["height"] / 2
-    ) ** 2 <= circle["radius"] ** 2
+        self.player.draw()
+        self.bricks.draw()
+        self.ball.draw()
 
 
 Game()
